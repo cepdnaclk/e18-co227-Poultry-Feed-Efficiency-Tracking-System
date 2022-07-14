3679b3f8c7722978ee58198e4fe421fe7c51b017
RishadMohamed <nmrishad6747@gmail.com>
2022-07-15 01:25:23 +0530

    Modified FCR Page

--- lib/screens/autoFCR.dart
+++ lib/screens/autoFCR.dart
@@ -1,17 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:home_login/screens/griddashboard.dart';
 
 class FCRAutoScreen extends StatefulWidget {
-  
-  FCRAutoScreen({Key? key}) : super(key: key);
+  // final String totalChicksNavi;
+  // final int mortalNavi;
+  // final num feedBagNavi;
+  // final num bagWeightNavi;
+  // final num avgWeightNavi;
+  final DateTime date;
+
+  //FCRAutoScreen({Key? key}) : super(key: key);
+  FCRAutoScreen({
+    Key? key,
+    // required this.totalChicksNavi,
+    // required this.mortalNavi,
+    // required this.feedBagNavi,
+    // required this.bagWeightNavi,
+    // required this.avgWeightNavi,
+    required this.date,
+  }) : super(key: key);
 
   @override
   State<FCRAutoScreen> createState() => _FCRAutoScreenState();
 }
 
 class _FCRAutoScreenState extends State<FCRAutoScreen> {
+  // late DateTime selectedDate;
+  // num feedbag = 0, bagWeight = 0;
+  // num avgWeight = 0;
   @override
   Widget build(BuildContext context) {
+    //final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
     return Scaffold(
       appBar: AppBar(
         title: Text("Auto"),
