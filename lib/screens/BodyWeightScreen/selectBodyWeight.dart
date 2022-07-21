696bf8c365a84ad738447642c2b2132745561844
RishadMohamed <nmrishad6747@gmail.com>
2022-07-21 23:52:32 +0530

    Body Weight

---
+++ lib/screens/BodyWeightScreen/selectBodyWeight.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectBodyWeight extends StatefulWidget {
+  SelectBodyWeight({Key? key}) : super(key: key);
+
+  @override
+  State<SelectBodyWeight> createState() => _SelectBodyWeightState();
+}
+
+class _SelectBodyWeightState extends State<SelectBodyWeight> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
